4382d0f6c8793610cc168b18d36277061151fb0e
lucas <lucas@digityze.asia>
2023-11-06 22:20:28 +0700

    [Fix]-selected_tags

---
+++ database/migrations/2023_11_06_204011_create_themes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('themes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index();
+            $table->string('display_name');
+            $table->string('preview_image')->nullable();
+            $table->string('author');
+            $table->string('package_name');
+            $table->text('value')->nullable();
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('themes');
+    }
+};
